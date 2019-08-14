4b25fac4ef808bb7a2a826aab13cea6a99a5f2b4
amante <bulidiriba@gmail.com>
2019-08-14 11:08:25 +0300

    remove changed folder

--- client_libraries/snet/face-detect/python/call.py
+++
@@ -1,8 +0,0 @@
-import snet
-#import snet.example_service as example_service
-
-from snet_sdk import Snet
-
-private_key = open('../../private_key.txt').readline()
-
-identity = Snet(private_key=private_key)
